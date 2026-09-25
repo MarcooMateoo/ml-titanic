9de59322a245644c3eba50f85e4c670173beec8c
Marco Mateo <marco.mateo.23.13@gmail.com>
2026-09-25 23:15:34 +0800

    modified:   .gitignore \        new file:   .ipynb_checkpoints/README-checkpoint.md \        modified:   notebooks/01_EDA.ipynb \        new file:   src/.ipynb_checkpoints/feature_engineering-checkpoint.py        new file:   src/.ipynb_checkpoints/preprocessing-checkpoint.py        new file:   src/feature_engineering.py        modified:   src/preprocessing.py

---
+++ src/.ipynb_checkpoints/feature_engineering-checkpoint.py
@@ -0,0 +1,12 @@
+# Here are the list of features that could be a condidate for producing good signal
+# This is a living list while doing EDA
+
+# 1. Family_size = SibSp + Parch + 1
+# 2. Is_solo = True If Family_size = 1 
+# 3. Age_between_20_30
+# 3. Interaction features
+#  a. Class_1_female
+#  b. Class_2_male
+#  c. Class_2_female
+#  d. Class_3_male
+#  e. Embarked_S_male
