acc871468695e2bc40423adc69ab52b1dcf6f289
Marco Mateo <marco.mateo.23.13@gmail.com>
2026-09-26 14:43:42 +0800

    Changes to be committed: 	modified:   notebooks/01_EDA.ipynb 	modified:   src/.ipynb_checkpoints/feature_engineering-checkpoint.py 	modified:   src/feature_engineering.py

--- src/.ipynb_checkpoints/feature_engineering-checkpoint.py
+++ src/.ipynb_checkpoints/feature_engineering-checkpoint.py
@@ -3,10 +3,23 @@
 
 # 1. Family_size = SibSp + Parch + 1
 # 2. Is_solo = True If Family_size = 1 
-# 3. Age_between_20_30
+# 3. Fare_bucket by 50 intervals
+# 4. Age_bucket by 10 intervals
+# 5. Deck = Cabin[0]
+# 6. Is_deck_missing = True if Deck = U
 # 3. Interaction features
 #  a. Class_1_female
 #  b. Class_2_male
 #  c. Class_2_female
 #  d. Class_3_male
 #  e. Embarked_S_male
+#  f. Embarked_S_female
+#  g. Embarked_C_c3
+#  h. Embarked_S_Age_21-30
+#  i. Class_3_Age_21-30
+#  j. Male_Age_21-30
+#  k. Female_Age_21-30
+#  l. Female_Age_31-40
+#  m. Class_3_Deck_U
+#  n. Male_Deck_U
+#  o. Embarked_S_Deck_U
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -5,7 +5,11 @@
 # 2. Is_solo = True If Family_size = 1 
 # 3. Fare_bucket by 50 intervals
 # 4. Age_bucket by 10 intervals
-# 3. Interaction features
+# 5. Deck = Cabin[0]
+# 6. Is_deck_missing = True if Deck = U
+# 7. Title = common title Mr, Miss, Mrs, Master
+# 8. 
+# Interaction features
 #  a. Class_1_female
 #  b. Class_2_male
 #  c. Class_2_female
@@ -13,3 +17,14 @@
 #  e. Embarked_S_male
 #  f. Embarked_S_female
 #  g. Embarked_C_c3
+#  h. Embarked_S_Age_21-30
+#  i. Class_3_Age_21-30
+#  j. Male_Age_21-30
+#  k. Female_Age_21-30
+#  l. Female_Age_31-40
+#  m. Class_3_Deck_U
+#  n. Male_Deck_U
+#  o. Embarked_S_Deck_U
+#  p. Class_3_Solo
+#  q. Male_Solo
+#  r. Embarked_S_Solo
